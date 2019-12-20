afb3b7736edc40f8a136f1e0eeab1550e32c10bc
alex-png <alexrod102797@gmail.com>
2019-12-20 12:28:28 -0500

    second test

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,12 @@ import React from 'react';
  
 class Home extends React.Component {
   render() {
-    return (<div>
+    return (
+    <>
+    <div>
       <h1>Home!</h1>
     </div>
+    </>
     )
   }
 }
